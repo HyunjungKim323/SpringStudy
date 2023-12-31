a930c6fe02caac160fce0abef8379efa20c2908b
HyunjungKim323 <119280993+HyunjungKim323@users.noreply.github.com>
2023-12-31 19:34:37 +0900

    Add files via upload

---
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,44 @@
+package hello.hellospring;
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+@Configuration
+public class SpringConfig {
+
+    //private final EntityManager em;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    /*@Autowired
+    public SpringConfig( EntityManager em) {
+        this.em = em;
+    }
+    */
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+    //@Bean
+    //public MemberRepository memberRepository() {
+    // return new MemoryMemberRepository();
+    // return new JdbcMemberRepository(dataSource);
+    // return new JdbcTemplateMemberRepository(dataSource);
+        // return new JpaMemberRepository(em);
+    //}
+    /*Component 사용
+    @Bean
+    public TimeTraceAop timeTraceAop() {
+        return new TimeTraceAop();
+    }*/
+
+}
